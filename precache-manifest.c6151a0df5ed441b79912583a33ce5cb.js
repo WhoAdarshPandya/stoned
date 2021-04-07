0da9f1380271ad58a5c2f7aa326e228ac403f6af
whoadarshpandya <adarshpandya0@gmail.com>
2021-04-08 02:09:58 +0530

    Updates

---
+++ precache-manifest.c6151a0df5ed441b79912583a33ce5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36650e8dec221cf899fb4e7df41a1965",
+    "url": "./index.html"
+  },
+  {
+    "revision": "748070efc6bf27409868",
+    "url": "./static/css/main.5d87d026.chunk.css"
+  },
+  {
+    "revision": "fd2b92c788e32dc197a0",
+    "url": "./static/js/2.7b10a038.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7b10a038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748070efc6bf27409868",
+    "url": "./static/js/main.4ad51bb7.chunk.js"
+  },
+  {
+    "revision": "ead3d794f98880a3ca65",
+    "url": "./static/js/runtime-main.196efef8.js"
+  }
+]);
